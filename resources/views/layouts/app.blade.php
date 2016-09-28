d8c6438e8173f319798b52f62eb1d31ad9fc894f
Andrea Swick <andrea@swicksolution.com>
2016-09-28 13:54:04 -0500

    Add visits and items to navbar, add auth protection around routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,8 @@
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @else
+                        <li><a href="/visits">Visits</a></li>
+                        <li><a href="/items">Items</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
--- routes/web.php
+++ routes/web.php
@@ -17,23 +17,25 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index');
 
-Route::get('/items', 'ItemsController@index')->name('items.index');
-Route::post('/items', 'ItemsController@store')->name('items.store');
-Route::get('/items/create', 'ItemsController@create')->name('items.create');
-Route::get('/items/{item}', 'ItemsController@show')->name('items.show');
-Route::delete('/items/{item}', 'ItemsController@destroy')->name('items.destroy');
-Route::patch('/items/{item}', 'ItemsController@update')->name('items.update');
-Route::get('/items/{item}/edit', 'ItemsController@edit')->name('items.edit');
-Route::get('/items/{item}/purchased', 'ItemsController@purchased');
+    Route::get('/items', 'ItemsController@index')->name('items.index');
+    Route::post('/items', 'ItemsController@store')->name('items.store');
+    Route::get('/items/create', 'ItemsController@create')->name('items.create');
+    Route::get('/items/{item}', 'ItemsController@show')->name('items.show');
+    Route::delete('/items/{item}', 'ItemsController@destroy')->name('items.destroy');
+    Route::patch('/items/{item}', 'ItemsController@update')->name('items.update');
+    Route::get('/items/{item}/edit', 'ItemsController@edit')->name('items.edit');
+    Route::get('/items/{item}/purchased', 'ItemsController@purchased');
 
-Route::get('/visits', 'VisitsController@index')->name('visits.index');
-Route::post('/visits', 'VisitsController@store')->name('visits.store');
-Route::get('/visits/create', 'VisitsController@create')->name('visits.create');
-Route::get('/visits/{visit}', 'VisitsController@show')->name('visits.show');
-Route::delete('/visits/{visit}', 'VisitsController@destroy')->name('visits.destroy');
-Route::patch('/visits/{visit}', 'VisitsController@update')->name('visits.update');
-Route::get('/visits/{visit}/edit', 'VisitsController@edit')->name('visits.edit');
+    Route::get('/visits', 'VisitsController@index')->name('visits.index');
+    Route::post('/visits', 'VisitsController@store')->name('visits.store');
+    Route::get('/visits/create', 'VisitsController@create')->name('visits.create');
+    Route::get('/visits/{visit}', 'VisitsController@show')->name('visits.show');
+    Route::delete('/visits/{visit}', 'VisitsController@destroy')->name('visits.destroy');
+    Route::patch('/visits/{visit}', 'VisitsController@update')->name('visits.update');
+    Route::get('/visits/{visit}/edit', 'VisitsController@edit')->name('visits.edit');
 
-Route::post('/visits/{visit}/comments', 'CommentsController@store')->name('comments.store');
+    Route::post('/visits/{visit}/comments', 'CommentsController@store')->name('comments.store');
+});
