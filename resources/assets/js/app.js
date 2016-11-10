55a216950e33837690645165a101a35941fb78ff
Andrea Swick <andrea@swicksolution.com>
2016-11-10 11:40:01 -0600

    Add smooth scroll for docs

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -27,3 +27,18 @@ $('.select2-multiple').select2();
 $('.fullcalendar').fullCalendar({
     events: '/api/visits'
 })
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 70
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
